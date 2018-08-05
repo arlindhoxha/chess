4bbe8316b49bdf583aa38a327728bf90255742f2
arlindhoxha <arlindhoxha007@gmail.com>
2018-08-05 19:45:33 -0400

    Implemented Bishop movement. Full junit coverage. Added TestSuite class

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,18 @@
+import com.ah.piece.BishopTest;
+import com.ah.piece.PawnTest;
+import com.ah.piece.RookTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Arlind Hoxha on 8/5/2018.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        PawnTest.class,
+        RookTest.class,
+        BishopTest.class
+})
+public class TestSuite {
+}
